192a8e4c8592a073881a34765157e59800c1a0e5
tho <dduongthitho@gmail.com>
2015-01-27 15:14:06 +0700

    Changed

--- ExerciseWebRole2/Global.asax.cs
+++ ExerciseWebRole2/Global.asax.cs
@@ -16,6 +16,8 @@ namespace ExerciseWebRole2
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+           
         }
     }
 }
